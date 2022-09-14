975b942cacf4ed802d875448d19dfdae65d8ecc8
s3723282 <s3723282@student.rmit.edu.au>
2022-09-14 20:02:55 +1000

    All tasks implemented into all 3 days
    Got all sets of tasks into all 3 days. Only issue is the open/close sign on day 2 and 3 not working.

--- Assets/Days/Day 2/Day2Outcomes.cs
+++ Assets/Days/Day 2/Day2Outcomes.cs
@@ -9,6 +9,7 @@ public class Day2Outcomes : Outcomes
     private TaskManager tm;
     private DialogueManager dm;
     private MedsEffects cameraEffects;
+    [SerializeField] private Animator signAnim;
 
     // Start is called before the first frame update
     void Start()
@@ -37,6 +38,11 @@ public class Day2Outcomes : Outcomes
                 break;
             case 1:
                 // player flipped sign to "closed" on front door
+                if (signAnim)
+                {
+                    //signAnim.SetBool("StartAnimation", true);
+                    signAnim.SetTrigger("TriggerAnimation");
+                }
                 // activate task board task
                 tm.ActivateTask(2);
                 // flip sign model here
@@ -64,7 +70,38 @@ public class Day2Outcomes : Outcomes
                 tm.ActivateTask(5);
                 break;
             case 6:
-                // player returned vacuum cleaner to storage room, level end
+                // prompt player to check taskboard again
+                tm.ActivateTask(6);
+                break;
+            case 7:
+                // prompt player to grab poop scooper
+                tm.ActivateTask(7);
+                break;
+            case 8:
+                //player picked up scooper
+                //activate tasks to scoop all the poop
+                tm.ActivateGroup(1);
+                break;
+            case 9:
+                // player scoops (all) poop
+                //player prompted to return scooper
+                tm.ActivateTask(8);
+                break;
+            case 10:
+                // prompt player to check taskboard for 3rd time
+                tm.ActivateTask(9);
+                break;
+            case 11:
+                //prompt player to collect dishes
+                tm.ActivateGroup(2);
+                break;
+            case 12:
+                //player collected dishes
+                //tell player to put dishes into sink
+                tm.ActivateTask(10);
+                break;
+            case 13:
+                // player completed all task for the day, level end
                 gm.LoadNextScene();
                 break;
             default:
