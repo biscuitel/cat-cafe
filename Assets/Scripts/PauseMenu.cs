1fcc75a40d9ced5fa8685d115719eefadbfe0a83
LilyPykee <110505699+LilyPykee@users.noreply.github.com>
2022-09-23 00:15:17 +1000

    PauseMenu works correctly on Day 2 and 3
    EventSystem GameObject is missing the Standalone Input Module Script component for the buttons.
    
    Added EventSystem->Add Component ->Standalone Input Module to buttons and all is working for Day 2 and 3.
    
    Also, ensured that Resume() was called from the Resume() button.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -27,6 +27,7 @@ public class PauseMenu : MonoBehaviour
 
     public void Pause()
     {
+        Debug.Log("Pause");
         //Show the Pause Menu, Pause the Game, and allow the cursor to be used
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
@@ -36,14 +37,12 @@ public class PauseMenu : MonoBehaviour
     
     public void Resume()
     {
+        Debug.Log("Resume");
         //Hide the Pause Menu, Start the Game, and hide the cursor
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
         Cursor.lockState = CursorLockMode.Locked;
-        //Cursor.lockState = false;
-        //GetComponent<MouseLook>().enabled = true;
-
     }
 
     public void MainMenu()
