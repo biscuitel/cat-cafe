728657c5a500d0c2fb9d7bd4176a07916a5924f9
xavibell <s3898724@student.rmit.edu.au>
2022-09-30 21:48:00 +1000

    Hair particles now play when hair is vacuumed
    I did it in a separate script since it was easier. Essentially, when the hair is destroyed, the hair particle system (which is always in the scene) is moved to the position of the hair and played once. Simple. I don't think it would make sense to have particles for the poop or cups, so I don't think there's any more work needed here.

---
+++ Assets/Scripts/HairParticles.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HairParticles : MonoBehaviour
+{
+    private ParticleSystem particles;
+    private GameObject particlesParent;
+    void Start()
+    {
+        particlesParent = GameObject.FindGameObjectWithTag("HairParticles");
+        particles = particlesParent.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnDestroy()
+    {
+        particlesParent.transform.position = this.transform.position;
+        particles.Play();
+    }
+}
