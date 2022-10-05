96352de809abf79f99a4ec0722d8efbe366f84eb
xavibell <s3898724@student.rmit.edu.au>
2022-10-05 16:59:00 +1100

    Vans (with satellite dish that follows player) added to day 2 and 3
    It's a little hard to notice the dish following you on day three, so I may change the window a little in the future.

---
+++ Assets/Scripts/LookAtPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    private GameObject player;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z));
+        
+    }
+}
