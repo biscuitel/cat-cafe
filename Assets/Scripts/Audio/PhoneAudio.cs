b4e2d926854548bdd6c3fbb38ab6094191502595
s3723282 <s3723282@student.rmit.edu.au>
2022-10-14 16:34:57 +1100

    Phone ringing audio synced
    Made a tiny script to play on an event in the phone animation so that the sound and animation will play in sync. This messed up the other sounds that play when interacting with the phone though and I have no idea how that was set up.

---
+++ Assets/Scripts/Audio/PhoneAudio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneAudio : MonoBehaviour
+{
+    AudioSource Audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void play_sound()
+    {
+        Audio.Play();
+    }
+}
