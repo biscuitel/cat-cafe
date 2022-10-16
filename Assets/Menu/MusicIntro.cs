b7dfea8f96d3172bce2a031bb22ccdb4c1b6cd1f
xavibell <s3898724@student.rmit.edu.au>
2022-10-16 16:37:57 +1100

    Added menu music

---
+++ Assets/Menu/MusicIntro.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicIntro : MonoBehaviour
+{
+
+    private AudioSource intro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        intro = GameObject.FindGameObjectWithTag("MusicIntro").GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!intro.isPlaying)
+        {
+            if (!this.GetComponent<AudioSource>().isPlaying)
+            {
+                this.GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+}
